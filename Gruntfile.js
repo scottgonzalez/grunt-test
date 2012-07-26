e0f247618a457a074f19743ee2e670aa940c408f
Scott González <scott.gonzalez@gmail.com>
2012-07-26 17:28:30 -0400

    Pure Grunt Goodness.

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:grunt-test.jquery.json>',
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    concat: {
+      dist: {
+        src: ['<file_strip_banner:src/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: '<config:concat.dist.dest>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    lint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        src: ['src/**/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<config:lint.gruntfile.src>',
+        tasks: ['lint:gruntfile']
+      },
+      src: {
+        files: '<config:lint.src.src>',
+        tasks: ['lint:src', 'qunit']
+      },
+      test: {
+        files: '<config:lint.test.src>',
+        tasks: ['lint:test', 'qunit']
+      },
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['lint', 'qunit', 'concat', 'min']);
+
+};
